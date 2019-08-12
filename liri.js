851bdf9c8505200b1f2690bc28870f64be38168e
Austin Alber <austinalber@gmail.com>
2019-08-12 17:15:33 -0400

    Completed concertThis function

--- liri.js
+++ liri.js
@@ -5,8 +5,8 @@ require("dotenv").config();
 var keys = require("./keys.js");
 
 // Variables
-var spotify = new Spotify(keys.spotify);
 var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
 var moment = require('moment');
 var axios = require('axios');
 var fs = require("fs");
@@ -46,17 +46,56 @@ switch(method) {
 
 // Functions
 function concertThis(userInput) {
-    var queryUrl = "https://rest.bandsintown.com/artists/" + inputParameter + "/events?app_id=codingbootcamp";
-};
+    // Use axios to retrieve bandsintown URL
+    axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp")
+    .then(function(response) {
+        // For loops to go through all possible entries from response
+        for(var i = 0; i < response.data.length; i++) {
+
+            // Save date of event as a variable, and split it into YYYY-MM-DD format
+            var date = (response.data[i].datetime).split("T");
+
+            // Display information to terminal
+            console.log(
+                "--------------------" +
+                "\nVenue name: " + response.data[i].venue.name + " " + response.data[i].venue.city + ", " + response.data[i].venue.country +
+                "\nDate of event: " + moment(date[0], "YYYY-MM-DD").format("dddd, MMMM Do YYYY") + " at " + moment(date[1], "hh:mm:ss").format("h:mm a") + "\n");
+        }
+    })
+    // Print error if axios grab is unsuccessful
+    .catch(function(error) {
+        console.log(error);
+    });
+}
 
 function spotifyThisSong(userInput) {
-
-};
+    // If no userInput is detected, use default song
+    if(!userInput) {
+        userInput = "The Sign";
+    }
+    spotify.search({
+        type: "track",
+        query: userInput
+    }).then(function(response) {
+        // For loops to go through all possible entries from response
+        console.log(response.tracks);
+        for(var i = 0; i < 5; i++) {
+            // Display song information to terminal
+            // console.log(
+            //     "--------------------" +
+            //     "Artist(s): " + 
+            // );
+        }
+    // If spotify search fails, display error
+    }).catch(function(error) {
+        console.log(error);
+    })
+}
 
 function movieThis(userInput) {
 
-};
+}
 
 function doWhatItSays() {
 
-};
+}
