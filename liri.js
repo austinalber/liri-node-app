9affed90e9ede6fbbe1e9ed5eae36d59f572223e
Austin Alber <austinalber@gmail.com>
2019-08-12 19:34:09 -0400

    Added LIRI() and inputCheck()

--- liri.js
+++ liri.js
@@ -15,35 +15,11 @@ var fs = require("fs");
 var method = process.argv[2];
 var userInput = process.argv[3];
 
-// Switch Statement for Methods
-switch(method) {
-    // Search Bands in Town Artits Events API
-    case "concert-this":
-        concertThis(userInput);
-    break;
+// If input is greater than one word, compress into one string
+inputCheck();
 
-    // Show Information About the Song in Terminal
-    case "spotify-this-song":
-        spotifyThisSong(userInput);
-    break;
-
-    // Show Information About the Movie in Terminal
-    case "movie-this":
-        movieThis(userInput);
-    break;
-
-    // Take Information From 'random.txt' and use it to Call one of LIRI's Commands
-    // Default will run 'spotify-this-song'
-    case "do-what-it-says":
-        doWhatItSays();
-    break;
-
-    // If no method is selected, display error to user and show potential methods
-    default:
-        console.log(
-            "\nIncorrect selection. Please choose from one of the following methods:\n" + 
-            "\n*  concert-this" + "\n*  spotify-this-song" + "\n*  movie-this" + "\n*  do-what-it-says\n");
-}
+// Run LIRI function
+LIRI();
 
 // Functions
 function concertThis(userInput) {
@@ -109,7 +85,7 @@ function movieThis(userInput) {
     .then(function(response) {
         // Display information to terminal
         console.log(
-            "--------------------" +
+            "\n--------------------" +
             "\nMovie Title: " + response.data.Title +
             "\nRelease Year: " + response.data.Year +
             "\nIMDB rating: " + response.data.imdbRating +
@@ -117,7 +93,7 @@ function movieThis(userInput) {
             "\nCountry Produced in: " + response.data.Country +
             "\nLanguage: " + response.data.Language + 
             "\nMovie Plot: " + response.data.Plot +
-            "\nActors in Movie: " + response.data.Actors);
+            "\nActors in Movie: " + response.data.Actors + "\n");
     })
     // Print error if axios grab is unsuccessful
     .catch(function(error) {
@@ -152,3 +128,43 @@ function doWhatItSays() {
     })
 }
 
+function inputCheck() {
+    if(process.argv.length > 4) {
+        for(var i = 4; i < process.argv.length; i++) {
+            userInput = userInput + "+" + process.argv[i];
+        }
+    }
+    return userInput;
+}
+
+function LIRI() {
+    // Switch Statement for Methods
+    switch(method) {
+        // Search Bands in Town Artits Events API
+        case "concert-this":
+            concertThis(userInput);
+        break;
+
+        // Show Information About the Song in Terminal
+        case "spotify-this-song":
+            spotifyThisSong(userInput);
+        break;
+
+        // Show Information About the Movie in Terminal
+        case "movie-this":
+            movieThis(userInput);
+        break;
+
+        // Take Information From 'random.txt' and use it to Call one of LIRI's Commands
+        // Default will run 'spotify-this-song'
+        case "do-what-it-says":
+            doWhatItSays();
+        break;
+
+        // If no method is selected, display error to user and show potential methods
+        default:
+            console.log(
+                "\nIncorrect selection. Please choose from one of the following methods:\n" + 
+                "\n*  concert-this" + "\n*  spotify-this-song" + "\n*  movie-this" + "\n*  do-what-it-says\n");
+    }
+}
